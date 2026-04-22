8095de4cf79a29803cb1bf7c4c86554dd784dcbb
Japser Zhang <58722459+VainJoker@users.noreply.github.com>
2026-04-22 05:45:49 +0000

    feat: Generate openapi docs

--- lib/segment/src/types.rs
+++ lib/segment/src/types.rs
@@ -2599,7 +2599,7 @@ pub struct MatchExcept {
     pub except: AnyVariants,
 }
 
-/// Parameters for fuzzy (approximate) full-text matching.
+/// Parameters for fuzzy full-text matching.
 #[derive(Debug, Deserialize, Serialize, JsonSchema, Validate, Copy, Clone, PartialEq, Eq, Hash)]
 #[serde(rename_all = "snake_case")]
 pub struct FuzzyParams {
